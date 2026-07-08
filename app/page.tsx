dfd67b064a0569eecb8ea0f536f1e6c1385fcd17
Muhammad Bilal <bilalanwer447@gmail.com>
2026-07-08 05:13:45 +0000

    Add Next.js app, Dockerfile, compose.yaml for Jenkins

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "100px", fontFamily: "sans-serif" }}>
+      <h1>Cloud Engineer Intern Assessment</h1>
+      <p>By: Muhammad Bilal</p>
+    </main>
+  );
+}
